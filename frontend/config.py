312fcb2e3284b65f756a89da58b2bbc93040ee3e
Abdullahaml1 <abdullah_aml@protonmail.com>
2024-08-23 16:12:12 +0300

    fix: typo

--- frontend/config.py
+++ frontend/config.py
@@ -1,6 +1,20 @@
-from utils import text_to_list
 from pathlib import Path
 import re
+
+
+def text_to_list(text: str, line_determiner: re.Pattern = re.compile(r'^(http)|\d+')):
+    """Creates a text area with each line is a sperate item identified by `line_determiner`
+
+    Args:
+        line_determiner (str): the re pattern that each line should be with EX: re.compile(r'^http')
+    """
+    text_list: list[str] = text.split('\n')
+    clean_text_list = []
+    for text in text_list:
+        if line_determiner.match(text):
+            clean_text_list.append(text)
+    return clean_text_list
+
 
 BASE_DIR = Path('DEMO_DIR')
 RECITER_POOL_FILE = BASE_DIR / 'reciter_pool.jsonl'
@@ -30,17 +44,3 @@ REQUIRED_MOSHAF_FIELDS = [
 MOSHAF_FIELD_FUNCS_AFTER_SUBMIT = {
     'sources': text_to_list
 }
-
-
-def text_to_list(text: str, line_determiner: re.Pattern = re.compile(r'^(http)|\d+')):
-    """Creates a text area with each line is a sperate item identified by `line_determiner`
-
-    Args:
-        line_determiner (str): the re pattern that each line should be with EX: re.compile(r'^http')
-    """
-    text_list: list[str] = text.split('\n')
-    clean_text_list = []
-    for text in text_list:
-        if line_determiner.match(text):
-            clean_text_list.append(text)
-    return clean_text_list
--- frontend/pages/view_moshaf_pool_page.py
+++ frontend/pages/view_moshaf_pool_page.py
@@ -20,12 +20,16 @@ def view_moshaf_pool():
                 st.markdown(
                     f'<span style="color: orange; font-weight: bold;">{label}: </span>{value}', unsafe_allow_html=True)
 
-            col1, col2 = st.columns(2)
+            col1, col2, col3 = st.columns(3)
             with col1:
                 if st.button("Update", key=f"update_{moshaf.id}", use_container_width=True):
                     st.session_state.updated_moshaf = moshaf
                     st.switch_page("pages/update_moshaf_page.py")
             with col2:
+                if st.button("Download", key=f"download_{moshaf.id}", use_container_width=True):
+                    pass
+
+            with col3:
                 st.button(
                     "Delete", key=f"delete_{moshaf.id}", use_container_width=True,
                     on_click=delete_item_from_pool_with_confirmation,
