54b23617918e28634acad882aebec7e43d5e5bd1
obadx <abdullahamlyossef@gmail.com>
2025-06-03 14:53:55 +0300

    add: tarteel transcription

--- src/prepare_quran_dataset/annotate/tarteel.py
+++ src/prepare_quran_dataset/annotate/tarteel.py
@@ -1,5 +1,10 @@
 from typing import Any
 from dataclasses import dataclass
+import io
+import asyncio
+
+from openai import AsyncOpenAI
+import soundfile as sf
 
 import numpy as np
 
@@ -36,16 +41,113 @@ def chunk_wave(
     return wav_chunks
 
 
-def merge_transcripts(trans: list[str]) -> str: ...
+async def transcribe_chunk(
+    client: AsyncOpenAI,
+    chunk: np.ndarray,
+    sample_rate=16000,
+    model="tarteel-ai/whisper-base-ar-quran",
+) -> str:
+    buffer = io.BytesIO()
+
+    # Handle different dtypes and ensure proper audio format
+    if chunk.dtype != np.int16 and chunk.dtype != np.float32:
+        if np.issubdtype(chunk.dtype, np.integer):
+            chunk = chunk.astype(np.float32) / np.iinfo(chunk.dtype).max
+        else:
+            chunk = chunk.astype(np.float32)
+
+    # Write audio to buffer
+    sf.write(
+        buffer,
+        chunk,
+        sample_rate,
+    )
+    wav_bytes = buffer.getvalue()
+
+    # Send request to ASR endpoint
+    out = await client.audio.transcriptions.create(
+        model=model,
+        file=wav_bytes,
+    )
+
+    return out.text
+
+
+def compute_prefix_function(pattern):
+    pi = [0] * len(pattern)
+    k = 0
+    for q in range(1, len(pattern)):
+        while k > 0 and pattern[k] != pattern[q]:
+            k = pi[k - 1]
+        if pattern[k] == pattern[q]:
+            k += 1
+        pi[q] = k
+    return pi
+
+
+class SmallTarteelOverlap(Exception): ...
+
+
+def merge_transcripts(
+    texts,
+    min_merge_chars=5,
+    end_truncate_words=1,
+    start_truncate_words=1,
+):
+    """Merge overlaping transcripts using Knuth-Morris-Pratt (KMP) pi Algorithm
+    https://cp-algorithms.com/string/prefix-function.html
+
+    Args:
+        text (list[str]):
+        min_merge_chars (int): the minum overlap length else raises SmallTarteelOverlap
+        end_truncate_words (int): removes the `end_truncate_words` form the end of every
+            transcript to avoid partial or wrong tnrascript  at the end of the chunked word
+        start_truncate_words (int): removes the `start_truncate_words` form the start of every
+            transcript to avoid partial or wrong tnrascript  at the start of the chunked word
+    """
+    # Removing trailing words and starting words
+    # to avoide (part of word wrong transcrips)
+    if len(texts) > 1:
+        # Not removing the start of the first transcript
+        words = texts[0].split(" ")
+        texts[0] = " ".join(words[: len(words) - end_truncate_words])
+        for idx in range(1, len(texts) - 1):
+            words = texts[idx].split(" ")
+            texts[idx] = " ".join(
+                words[start_truncate_words : len(words) - end_truncate_words]
+            )
+        # Not removing the tail of the last transcript
+        texts[-1] = " ".join(texts[-1].split(" ")[start_truncate_words:])
+
+    if not texts:
+        return ""
+    merged = texts[0]
+    for i in range(1, len(texts)):
+        next_text = texts[i]
+        L = min(len(merged), len(next_text))
+        s_tail = merged[-L:]
+        pi = compute_prefix_function(next_text)
+        state = 0  # number of matched chars
+        for char in s_tail:
+            # going batch to tha last matching prefix
+            while state > 0 and next_text[state] != char:
+                state = pi[state - 1]
+            if next_text[state] == char:
+                state += 1
+        if state < min_merge_chars:
+            raise SmallTarteelOverlap("Very Small overlap to merge on")
+        merged += next_text[state:]
+    return merged
 
 
 async def tarteel_transcript(
     wave: np.ndarray,
     sample_rate=16000,
     max_len_sec: float = 30,
-    chunck_overlap_sec: float = 10,
-    vllm_endpoint="",
-):
+    chunck_overlap_sec: float = 12,
+    vllm_endpoint="http://localhost:8000/v1",
+    model="tarteel-ai/whisper-base-ar-quran",
+) -> list[str]:
     assert chunck_overlap_sec < max_len_sec
     max_len_samples = int(max_len_sec * sample_rate)
     chunck_overlap_samples = int(chunck_overlap_sec * sample_rate)
@@ -56,7 +158,13 @@ async def tarteel_transcript(
     )
 
     transcripts: list[str] = []
-    # run parallel async requests
+    client = AsyncOpenAI(api_key="None", base_url="vllm_endpoint")
 
-    # merge transcripts
-    merged_trans = merge_transcripts(transcripts)
+    # Run concerent async requests
+    tasks = [
+        transcribe_chunk(client, chunk, model=model, sample_rate=sample_rate)
+        for chunk in wav_chunks
+    ]
+    transcripts = await asyncio.gather(*tasks)
+
+    return transcripts
--- tests/tarteel_pytest.py
+++ tests/tarteel_pytest.py
@@ -1,7 +1,11 @@
 import pytest
 import numpy as np
 
-from prepare_quran_dataset.annotate.tarteel import chunk_wave
+from prepare_quran_dataset.annotate.tarteel import (
+    chunk_wave,
+    merge_transcripts,
+    SmallTarteelOverlap,
+)
 
 
 @pytest.mark.parametrize(
@@ -60,3 +64,121 @@ def test_chunk_wave(wave, chunk_overlap, max_len_samples, expected):
     )
     for idx in range(len(expected)):
         np.testing.assert_equal(chunks[idx], expected[idx])
+
+
+@pytest.mark.parametrize(
+    "texts, expected, start, end",
+    [
+        (
+            [
+                "Ahmed Mohammed",
+                "Mohammed Othman",
+            ],
+            "Ahmed Mohammed Othman",
+            0,
+            0,
+        ),
+        (
+            [
+                "Ahmed Mohammed",
+                "Mohammed Othman",
+                "Othman Alsayed",
+            ],
+            "Ahmed Mohammed Othman Alsayed",
+            0,
+            0,
+        ),
+        (
+            [
+                "Ahmed Mohammed asdkfh",
+                "Mohammed Othman",
+            ],
+            "Ahmed Mohammed Othman",
+            0,
+            1,
+        ),
+        (
+            [
+                "Ahmed Mohammed",
+                "saldkfj Mohammed Othman",
+            ],
+            "Ahmed Mohammed Othman",
+            1,
+            0,
+        ),
+        (
+            [
+                "Ahmed Mohammed asdkfh",
+                "skldjfkj Mohammed Othman",
+            ],
+            "Ahmed Mohammed Othman",
+            1,
+            1,
+        ),
+        (
+            [
+                "Ahmed Mohammed",
+                "sldkfj  Mohammed Othman",
+                "sdlkfj Othman Alsayed",
+            ],
+            "Ahmed Mohammed Othman Alsayed",
+            1,
+            0,
+        ),
+        (
+            [
+                "Ahmed Mohammed sdkfj",
+                "Mohammed Othman slkdfj",
+                "Othman Alsayed",
+            ],
+            "Ahmed Mohammed Othman Alsayed",
+            0,
+            1,
+        ),
+        (
+            [
+                "Ahmed Mohammed sdlkfjs",
+                "lkdfj Mohammed Othman askdjf",
+                "sldkfj Othman Alsayed",
+            ],
+            "Ahmed Mohammed Othman Alsayed",
+            1,
+            1,
+        ),
+        (
+            [
+                "Ahmed Mohammed aslkdfj asdklfj",
+                "Mohammed Othman skdjf sdkjf",
+                "Othman Alsayed",
+            ],
+            "Ahmed Mohammed Othman Alsayed",
+            0,
+            2,
+        ),
+        (
+            [
+                "Ahmed Mohammed skldfj sldkfj",
+                "sdkfj sdklfj sdlkjf Mohammed Othman sdlkfj sldkfj",
+                "sldkfj sldkfj sldkfj Othman Alsayed",
+            ],
+            "Ahmed Mohammed Othman Alsayed",
+            3,
+            2,
+        ),
+    ],
+)
+def test_merge_text(texts, expected, start, end):
+    assert expected == merge_transcripts(
+        texts, min_merge_chars=5, start_truncate_words=start, end_truncate_words=end
+    )
+
+
+def test_merge_text_raise():
+    texts = [
+        "Mohammed Ahmed",
+        "Mohammed Othman",
+    ]
+    with pytest.raises(SmallTarteelOverlap):
+        merge_transcripts(
+            texts, min_merge_chars=5, start_truncate_words=0, end_truncate_words=0
+        )
