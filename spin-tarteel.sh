0bd598de5f63d0a8b48b012a84b3c55d60191cbd
obadx <abdullahamlyossef@gmail.com>
2025-06-04 17:33:10 +0300

    reducing tarteel utlization

--- spin-tarteel.sh
+++ spin-tarteel.sh
@@ -6,7 +6,7 @@ docker run --gpus all \
     	vllm-audio \
 	--no-enable-prefix-caching \
     	--model tarteel-ai/whisper-base-ar-quran \
-	--gpu-memory-utilization 0.9 \
+	--gpu-memory-utilization 0.6 \
 	--dtype float32 \
 	# --cpu-offload-gb 4 \
 	# --max-model-len 10 \
