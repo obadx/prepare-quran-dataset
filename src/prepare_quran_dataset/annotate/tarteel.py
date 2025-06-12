24f0ff077fe1561ff4e18259b126766897290af6
obadx <abdullahamlyossef@gmail.com>
2025-06-12 18:00:27 +0300

    add: stamina

--- src/prepare_quran_dataset/annotate/tarteel.py
+++ src/prepare_quran_dataset/annotate/tarteel.py
@@ -5,6 +5,7 @@ import asyncio
 
 from openai import AsyncOpenAI, APIConnectionError
 import soundfile as sf
+import stamina
 
 import numpy as np
 
@@ -140,6 +141,7 @@ def merge_transcripts(
     return merged
 
 
+@stamina.retry(on=APIConnectionError, attempts=3)
 async def tarteel_transcribe(
     wave: np.ndarray,
     sample_rate=16000,
