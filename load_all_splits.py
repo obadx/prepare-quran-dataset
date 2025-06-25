7b5e829203bb4acf05be90f93f2be7e5c42bf619
obadx <abdullahamlyossef@gmail.com>
2025-06-26 01:03:35 +0300

    add: loading all splits as jobs

--- load_all_splits.py
+++ load_all_splits.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from datasets import load_dataset
+import submitit
 
 from prepare_quran_dataset.construct.data_classes import Moshaf, Reciter
 from prepare_quran_dataset.construct.database import MoshafPool, ReciterPool
@@ -12,6 +13,26 @@ if __name__ == "__main__":
     reciter_pool = ReciterPool(ds_path / "reciter_pool.jsonl")
     moshaf_pool = MoshafPool(reciter_pool, ds_path)
 
+    # Configure Slurm
+    executor = submitit.AutoExecutor(folder="logs")
+    executor.update_parameters(
+        slurm_account="shams035",
+        slurm_partition="cpu",
+        slurm_time="0-4:00:00",
+        slurm_ntasks_per_node=1,
+        cpus_per_task=16,
+    )
+
     for moshaf in moshaf_pool:
-        ds = load_dataset(str(ds_path), name=f"moshaf_{moshaf.id}", split="train")
-        print(ds)
+        executor.update_parameters(
+            slurm_job_name=f"L-{moshaf.id}",
+            slurm_additional_parameters={
+                # "output": f"QVADcpu_{split}_%j.out"  # %j = Slurm job ID
+            },
+        )
+        job = executor.submit(
+            lambda: load_dataset(
+                str(ds_path), name=f"moshaf_{moshaf.id}", split="train"
+            )
+        )
+        print(job.job_id)
