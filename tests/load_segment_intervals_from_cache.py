b94aa73542c142ed7c932390054785cb29a55b92
obadx <abdullahamlyossef@gmail.com>
2025-06-28 02:00:09 +0300

    add: loading from cache script

--- tests/load_segment_intervals_from_cache.py
+++ tests/load_segment_intervals_from_cache.py
@@ -8,14 +8,14 @@ if __name__ == "__main__":
     reciter_pool = ReciterPool(ds_path / "reciter_pool.jsonl")
     moshab_pool = MoshafPool(reciter_pool, ds_path)
 
-    moshaf_id = "4.0"
-    segment_to_interval = load_segment_intervals_from_cache(
-        moshab_pool[moshaf_id],
-        f".segment_cache/{moshaf_id}",
-        ds_path / f"dataset/{moshaf_id}/metadata.jsonl",
-    )
-    print(len(segment_to_interval))
-    print(segment_to_interval["002.0409"])
+    # moshaf_id = "4.0"
+    # segment_to_interval = load_segment_intervals_from_cache(
+    #     moshab_pool[moshaf_id],
+    #     f".segment_cache/{moshaf_id}",
+    #     ds_path / f"dataset/{moshaf_id}/metadata.jsonl",
+    # )
+    # print(len(segment_to_interval))
+    # print(segment_to_interval["002.0409"])
 
     moshaf_id = "7.0"
     segment_to_interval = load_segment_intervals_from_cache(
@@ -25,12 +25,23 @@ if __name__ == "__main__":
     )
     print(len(segment_to_interval))
     print(segment_to_interval["002.0409"])
+    print(segment_to_interval["021.0151"])
+    print(segment_to_interval["021.0152"])
 
-    moshaf_id = "0.0"
-    segment_to_interval = load_segment_intervals_from_cache(
-        moshab_pool[moshaf_id],
-        f".segment_cache/{moshaf_id}",
-        ds_path / f"dataset/{moshaf_id}/metadata.jsonl",
-    )
-    print(len(segment_to_interval))
-    print(segment_to_interval["002.0409"])
+    # moshaf_id = "0.0"
+    # segment_to_interval = load_segment_intervals_from_cache(
+    #     moshab_pool[moshaf_id],
+    #     f".segment_cache/{moshaf_id}",
+    #     ds_path / f"dataset/{moshaf_id}/metadata.jsonl",
+    # )
+    # print(len(segment_to_interval))
+    # print(segment_to_interval["002.0409"])
+
+    # moshaf_id = "2.0"
+    # segment_to_interval = load_segment_intervals_from_cache(
+    #     moshab_pool[moshaf_id],
+    #     f".segment_cache/{moshaf_id}",
+    #     ds_path / f"dataset/{moshaf_id}/metadata.jsonl",
+    # )
+    # print(len(segment_to_interval))
+    # print(segment_to_interval["002.0409"])
