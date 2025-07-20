009bf07de6dcaa67591e342bfefa3c6ddd7418ed
obadx <abdullahamlyossef@gmail.com>
2025-07-21 01:14:00 +0300

    checking start span key

--- view_dataset_st.py
+++ view_dataset_st.py
@@ -669,15 +669,14 @@ def find_nearest_tasmeea_results(moshaf_id: str, sura_idx: int, aya_idx: int, wi
         for tasmeea_info in st.session_state.moshaf_to_sura_to_tasmeea[moshaf_id][
             sura_idx
         ]:
-            st.write(type(tasmeea_info))
-            st.write(tasmeea_info)
             for search_aya_idx in range(start_aya_idx, end_aya_idx + 1):
-                if tasmeea_info["start_span"] is not None:
-                    if (
-                        sura_idx == tasmeea_info["start_span"]["sura_idx"]
-                        and search_aya_idx == tasmeea_info["start_span"]["aya_idx"]
-                    ):
-                        seg_ids.append(tasmeea_info["segment_index"])
+                if "start_span" in tasmeea_info:
+                    if tasmeea_info["start_span"] is not None:
+                        if (
+                            sura_idx == tasmeea_info["start_span"]["sura_idx"]
+                            and search_aya_idx == tasmeea_info["start_span"]["aya_idx"]
+                        ):
+                            seg_ids.append(tasmeea_info["segment_index"])
     return seg_ids
 
 
