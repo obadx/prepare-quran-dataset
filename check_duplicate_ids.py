5c2c5c8f8e540262ba2bfca98d3b5de4fca9f715
obadx <abdullahamlyossef@gmail.com>
2025-07-20 15:35:27 +0300

    add: check for duplicate ids

---
+++ check_duplicate_ids.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+import json
+
+from datasets import load_dataset
+
+from prepare_quran_dataset.construct.database import MoshafPool, ReciterPool
+
+if __name__ == "__main__":
+    ds_path = Path("/cluster/users/shams035u1/data/mualem-recitations-annotated")
+    reciter_pool = ReciterPool(ds_path / "reciter_pool.jsonl")
+    moshaf_pool = MoshafPool(reciter_pool, ds_path)
+
+    moshaf_to_dup = {}
+    for moshaf in moshaf_pool:
+        ds = load_dataset(
+            str(ds_path), name=f"moshaf_{moshaf.id}", split="train", num_proc=32
+        )
+        seg_ids = ds["segment_index"]
+        segs = set()
+        for seg in seg_ids:
+            if seg not in segs:
+                segs.add(seg)
+            else:
+                if moshaf.id not in moshaf_to_dup:
+                    moshaf_to_dup[moshaf.id] = []
+                moshaf_to_dup[moshaf.id].append(seg)
+
+    with open("duplicate_segs.json", "w+") as f:
+        json.dump(moshaf_to_dup, indent=2)
