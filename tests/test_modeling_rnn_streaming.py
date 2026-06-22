48e19c384d3fac99e8b96ad33848897c36e9590a
obadx <abdullahamlyossef@gmail.com>
2026-06-22 20:56:02 +0300

    add: simple testing script

--- tests/test_modeling_rnn_streaming.py
+++ tests/test_modeling_rnn_streaming.py
@@ -1,10 +1,12 @@
 import json
 
 from quran_transcript import quran_phonetizer, Aya, MoshafAttributes
-from transformers import AutoFeatureExtractor
+from transformers import AutoFeatureExtractor, Wav2Vec2BertModel
 import torch
 
-from prepare_quran_dataset.modeling.multi_level_tokenizer import MultiLevelTokenizer
+from prepare_quran_dataset.modeling_streaming_rnn.multi_level_tokenizer import (
+    MultiLevelTokenizer,
+)
 from prepare_quran_dataset.modeling_streaming_rnn.configuration_rnn_streaming_multi_level_ctc import (
     Wav2Vec2BertForRNNStreamingMultilevelCTCConfig,
 )
@@ -20,105 +22,93 @@ if __name__ == "__main__":
 
     config = Wav2Vec2BertForRNNStreamingMultilevelCTCConfig(
         level_to_vocab_size=level_to_vocab_size,
+        hidden_size=512,
+        intermediate_size=2048,
+        num_hidden_layers=16,
+        rnn_hidden_size=128,
+        add_adapter=False,
+        apply_spec_augment=False,
+        mask_time_prob=0.0,
+        layerdrop=0.0,
     )
     print(config)
 
     model = Wav2Vec2BertForRNNStreamingMultilevelCTC.from_pretrained(
-        "facebook/w2v-bert-2.0", config=config
+        "facebook/w2v-bert-2.0",
+        config=config,
+        ignore_mismatched_sizes=True,
     )
+    # base_model = Wav2Vec2BertModel.from_pretrained(
+    #     "facebook/w2v-bert-2.0",
+    #     config=config,
+    #     ignore_mismatched_sizes=True,
+    # )
+
     print(model)
     processor = AutoFeatureExtractor.from_pretrained("facebook/w2v-bert-2.0")
     print(processor)
     batch_size = 2
 
-    # Test offline inference without labels
+    # Test Infernce without labels
     inputs = processor([[0] * 16000] * batch_size, return_tensors="pt")
-    input_features = inputs["input_features"]
-    attention_mask = inputs["attention_mask"]
-    model_output = model(input_features=input_features, attention_mask=attention_mask)
-    print(model_output)
+    model_output = model(**inputs)
+    # base_model_output = base_model(**inputs)
+    # print(f"\n\nBase model Output:\n{base_model_output}")
+    # print(model_output)
 
-    # Test offline inference with labels
-    tokenizer = MultiLevelTokenizer("./")
-
-    uth_strings = [Aya(2, i).get().uthmani for i in range(1, batch_size + 1)]
-    moshaf = MoshafAttributes(
-        rewaya="hafs",
-        madd_monfasel_len=4,
-        madd_mottasel_len=4,
-        madd_mottasel_waqf=4,
-        madd_aared_len=4,
-        tasheel_or_madd="madd",
-    )
-
-    photenized_outs = [
-        quran_phonetizer(uth_str, moshaf, remove_spaces=True) for uth_str in uth_strings
-    ]
-
-    token_out = tokenizer.tokenize(
-        [p.phonemes for p in photenized_outs],
-        [p.sifat for p in photenized_outs],
-        to_dict=True,
-        return_tensors="pt",
-        padding="longest",
-    )
-    print(token_out["input_ids"]["phonemes"].shape)
-
-    labels = token_out["input_ids"]
-    labels_mask = {level: (labels[level] >= 0).to(torch.long) for level in labels}
-
-    model_output = model(
-        input_features=input_features,
-        attention_mask=attention_mask,
-        labels=labels,
-        labels_mask=labels_mask,
-        return_dict=False,
-    )
-    print(model_output[1].keys())
-    print(model_output[1]["phonemes"].shape)
-    print(model_output[1]["phonemes"].argmax(dim=-1))
-
-    level_to_pred_ids = {k: torch.argmax(v, dim=-1) for k, v in model_output[1].items()}
-    print(level_to_pred_ids)
-    decoded_outs = tokenizer.decode(
-        level_to_pred_ids,
-        place_zeros_in_between=False,
-    )
-    print(json.dumps(decoded_outs, indent=1, ensure_ascii=False))
-
-    print(
-        model(
-            input_features=input_features,
-            attention_mask=attention_mask,
-            labels=labels,
-            labels_mask=labels_mask,
-        ).loss
-    )
-
-    # Test streaming inference (single chunk)
-    streaming_len = model.streaming_len
-    chunk = input_features[:batch_size, :streaming_len, :]
-    chunk_mask = attention_mask[:batch_size, :streaming_len]
-    streaming_output = model(
-        input_features=chunk,
-        attention_mask=chunk_mask,
-        stream_inference=True,
-    )
-    print(streaming_output)
-
-    # Test streaming inference with labels
-    chunk_labels = {
-        k: v[:, : streaming_output.logits[k].shape[1]] for k, v in labels.items()
-    }
-    chunk_labels_mask = {
-        k: v[:, : streaming_output.logits[k].shape[1]] for k, v in labels_mask.items()
-    }
-    print(
-        model(
-            input_features=chunk,
-            attention_mask=chunk_mask,
-            stream_inference=True,
-            labels=chunk_labels,
-            labels_mask=chunk_labels_mask,
-        ).loss
-    )
+    # # Test Inference with labels
+    # tokenizer = MultiLevelTokenizer("./")
+    #
+    # uth_strings = [Aya(2, i).get().uthmani for i in range(1, batch_size + 1)]
+    # moshaf = MoshafAttributes(
+    #     rewaya="hafs",
+    #     madd_monfasel_len=4,
+    #     madd_mottasel_len=4,
+    #     madd_mottasel_waqf=4,
+    #     madd_aared_len=4,
+    #     tasheel_or_madd="madd",
+    # )
+    #
+    # photenized_outs = [
+    #     quran_phonetizer(
+    #         uth_str,
+    #         moshaf,
+    #         remove_spaces=True,
+    #     )
+    #     for uth_str in uth_strings
+    # ]
+    #
+    # token_out = tokenizer.tokenize(
+    #     [p.phonemes for p in photenized_outs],
+    #     [p.sifat for p in photenized_outs],
+    #     to_dict=True,
+    #     return_tensors="pt",
+    #     padding="longest",
+    # )
+    # print(token_out["input_ids"]["phonemes"].shape)
+    #
+    # model_output = model(
+    #     **inputs,
+    #     labels=token_out["input_ids"],
+    #     labels_mask=token_out["attention_mask"],
+    #     return_dict=False,
+    # )
+    # print(model_output[1].keys())
+    # print(model_output[1]["phonemes"].shape)
+    # print(model_output[1]["phonemes"].argmax(dim=-1))
+    #
+    # level_to_pred_ids = {k: torch.argmax(v, dim=-1) for k, v in model_output[1].items()}
+    # print(level_to_pred_ids)
+    # decoded_outs = tokenizer.decode(
+    #     level_to_pred_ids,
+    #     place_zeros_in_between=False,
+    # )
+    # print(json.dumps(decoded_outs, indent=1, ensure_ascii=False))
+    #
+    # print(
+    #     model(
+    #         **inputs,
+    #         labels=token_out["input_ids"],
+    #         labels_mask=token_out["attention_mask"],
+    #     ).loss
+    # )
