dda79f13d05cdd5c1c4d9de6c9a0906c1520920a
Abdullah-Haytham <abdullah.haytham@silverkeytech.com>
2026-08-19 20:49:47 +0300

    Revert "Update __init__.py"
    This reverts commit 3b332702edd8a195c24a71918bb0aadaabe1ca48.

--- src/prepare_quran_dataset/modeling_fastconformer_cache_aware/__init__.py
+++ src/prepare_quran_dataset/modeling_fastconformer_cache_aware/__init__.py
@@ -1,25 +1,17 @@
 from .configuration_fastconformer_cache_aware import (
     FastConformerCacheAwareMultilevelCTCConfig,
 )
-from .inference import (
-    FastConformerCacheAwareMultilevelCTCInference,
-    stream_inference,
-)
 from .modeling_fastconformer_cache_aware_ctc import (
     FastConformerCache,
     FastConformerCacheAwareMultilevelCTC,
     FastConformerCTCWithCacheOutput,
 )
 from .processor import FastConformerMelProcessor
-from .streaming_buffer import HFCacheAwareStreamingAudioBuffer
 
 __all__ = [
     "FastConformerCache",
     "FastConformerCacheAwareMultilevelCTC",
     "FastConformerCacheAwareMultilevelCTCConfig",
-    "FastConformerCacheAwareMultilevelCTCInference",
     "FastConformerCTCWithCacheOutput",
     "FastConformerMelProcessor",
-    "HFCacheAwareStreamingAudioBuffer",
-    "stream_inference",
 ]
